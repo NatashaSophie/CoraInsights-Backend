8d528bfc9df7ccd4e38bcaba9855841cd4665c56
CoraApp Developer <natasha.sophie@gmail.com>
2026-02-01 12:16:54 -0300

    fix: Remover auth: true das rotas (Strapi V3 não suporta)

--- app/api/analytics/config/routes.js
+++ app/api/analytics/config/routes.js
@@ -10,7 +10,6 @@ module.exports = {
       path: '/analytics/pilgrim',
       handler: 'analytics.getPilgrimAnalytics',
       config: {
-        auth: true, // Requer autenticação
         policies: []
       }
     },
@@ -19,7 +18,6 @@ module.exports = {
       path: '/analytics/manager',
       handler: 'analytics.getManagerAnalytics',
       config: {
-        auth: true, // Requer autenticação
         policies: []
       }
     },
@@ -28,7 +26,6 @@ module.exports = {
       path: '/analytics/merchant',
       handler: 'analytics.getMerchantAnalytics',
       config: {
-        auth: true, // Requer autenticação
         policies: []
       }
     }
