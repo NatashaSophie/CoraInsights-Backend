d9cf64da2c4669bee1e94ba5cb094547b5fc28c9
CoraApp Developer <natasha.sophie@gmail.com>
2026-01-31 22:15:29 -0300

    chore: limpar repositório - remover arquivos de teste, cache e reorganizar scripts

--- app/check-cols.js
+++
@@ -1,8 +0,0 @@
-﻿const knex = require('knex')({
-  client: 'pg',
-  connection: { host: 'localhost', port: 5432, user: 'postgres', password: 'postgres', database: 'strapi' }
-});
-knex.raw(SELECT column_name FROM information_schema.columns WHERE table_name = 'trail_routes').then(r => {
-  console.log(r.rows.map(x => x.column_name));
-  process.exit();
-});
