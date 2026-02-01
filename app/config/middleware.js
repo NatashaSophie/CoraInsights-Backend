9e4e32b88615e0bb128775aaf20fbd6862ca89b5
CoraApp Developer <natasha.sophie@gmail.com>
2026-01-31 23:55:23 -0300

    feat: adicionar middleware customizado para autenticação

--- app/config/middleware.js
+++ app/config/middleware.js
@@ -1,8 +1,21 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
+module.exports = ({ env }) => [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'custom-auth',
+    config: {
       enabled: true,
-      origin: ['http://localhost:1337', 'http://localhost:3001', 'http://localhost:3000'],
     },
   },
-});
+  {
+    resolve: './middlewares/custom-auth',
+  },
+];
+
